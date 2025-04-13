7b93525d1c4299e95d6b63fdd0ae8e1b9007576e
tookiiooo0874 <hogarakaboy@gmail.com>
2025-04-13 15:02:49 +0900

    ch01 fin

---
+++ tokio_study/numpy_bool.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+X = np.array([1, 2, 11, 12, 21, 22])
+
+print(X > 15)
+print(X[X > 15])
+print(X > 15)
+print(X[False, False, False, False, True, True])
---
+++ tokio_study/numpy_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+C = np.array([1, 2, 3])
+
+print(A * B)
+print(A + B)
+print(A * C)
+print(B * C)
+print(B + C)
